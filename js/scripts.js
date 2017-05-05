5d8ffb1036a959f3e7c36af63e0b8e53f66c6cd9
tw1sterPL <xxgambrynusxx@gmail.com>
2017-05-05 21:06:16 +0200

    dodanie pola trójkata

--- js/scripts.js
+++ js/scripts.js
@@ -8,5 +8,18 @@ console.log('Witaj, ' + name);
   var name = prompt('Wpisz swoje ID');
   alert('Witaj, ' + name);
   console.log(‘Zakomentowana się nie wywoła’);
-  
+
 */
+
+// wzór na pole trójkąta
+var a = prompt('Podaj wartość a');
+var b = prompt('Podaj wartość h');
+
+var triangleArea = a*b/2;
+if ((triangleArea < 0)) {
+	alert("błąd, takiego pola nie da sie obliczyć - spróbuj ponownie");
+}
+else {
+alert('Pole wynosi, ' + triangleArea);
+console.log('Pole trójkąta o podstawie a: ' + a + ' i wysokości h: ' + h + ' wynosi: ' + triangleArea);
+}
