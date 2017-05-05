04b9cf0ded345a96fe1854e9a5b8c9f284ab5c6b
tw1sterPL <xxgambrynusxx@gmail.com>
2017-05-05 19:03:41 +0200

    zmiana kosmetyczna

--- js/scripts.js
+++ js/scripts.js
@@ -2,3 +2,10 @@
 var name = prompt('Wpisz swoje imię');
 alert('Witaj, ' + name);
 console.log('Witaj, ' + name);
+
+/*
+  komentarz blokowy
+  var name = prompt('Wpisz swoje ID');
+  alert('Witaj, ' + name);
+  console.log(‘Zakomentowana się nie wywoła’);
+*/
